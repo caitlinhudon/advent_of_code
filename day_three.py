3e436c49966d95ddc25ed56d10eec8862534de52
Caitlin Hudon <caitlingarrett8@gmail.com>
2020-12-03 20:51:39 -0600

    day three: second puzzle complete!

--- day_three.py
+++ day_three.py
@@ -2,18 +2,30 @@ import math
 with open('inputs/day_three.txt') as input:
     grid = input.read().splitlines()
 
-row = 0
-column = 0
-trees = 0
-for g in grid:
-    if row > 9:
-        expansions = math.ceil(column / 30)
-        expanded_g = g * expansions
-    else:
-        expanded_g = g
-    if expanded_g[column] == '#':
-        trees += 1
-    row += 1
-    column += 3
+def get_trees(over, down = 1):
+    row = 0
+    column = 0
+    trees = 0
+    for g in grid[::down]:
+        if row > 3:
+            expansions = math.ceil(column / 30)
+            expanded_g = g * expansions
+        else:
+            expanded_g = g
+        if expanded_g[column] == '#':
+            trees += 1
+        row += down
+        column += over
+    return trees
 
-print(trees)
+part_one_answer = get_trees(3)
+part_two_answer = get_trees(1) * get_trees(3) * get_trees(5) * get_trees(7) * get_trees(1, 2)
+print(part_one_answer)
+print(part_two_answer)
+
+
+# i've put in:
+3772314000
+
+# 6742008000 (too high)
+# 1314691560000 (too high)
