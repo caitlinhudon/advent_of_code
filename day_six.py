3e20cbc0577dd3e375dfd21f53a39ca6b9d9b618
Caitlin Hudon <caitlingarrett8@gmail.com>
2020-12-06 20:50:58 -0600

    day six: puzzle two completed! )

--- day_six.py
+++ day_six.py
@@ -3,8 +3,8 @@ from collections import Counter
 with open('inputs/day_six.txt') as input:
     data = input.read()
 
-group_answers = data.split('\n\n')
-group_answers = [str.replace(answer, '\n','') for answer in group_answers]
+group_answers_raw = data.split('\n\n')
+group_answers = [str.replace(answer, '\n','') for answer in group_answers_raw]
 
 all_counts = []
 for answer in group_answers:
@@ -15,3 +15,17 @@ print(sum(all_counts))
 
 # part two --------------------------------------------------------------------
 
+#for answer in group_answers:
+#    count = len(Counter(answer))
+#    all_counts.append(count)
+
+group_answers_two = [str.replace(answer, '\n',' ') for answer in group_answers_raw]
+
+all_answered = []
+for answer in group_answers_two:
+    counted = Counter(answer)
+    people = counted[' '] + 1
+    all_answered_qs = {x: count for x, count in counted.items() if count == people}
+    all_answered.append(len(all_answered_qs))
+
+print(sum(all_answered))
