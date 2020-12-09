105caaa0c4ddd1f0a8274b3869ac127cffa3e8de
Caitlin Hudon <caitlingarrett8@gmail.com>
2020-12-08 23:06:14 -0600

    day eight: puzzle two complete!

--- day_eight.py
+++ day_eight.py
@@ -1,30 +1,69 @@
 with open('inputs/day_eight.txt') as f:
     instructions = f.read().splitlines()
 
-milestones = []
-accumulator_values = []
-next_checks = [0]
-visited_count = [0] * len(instructions)
-for i, next_check in enumerate(next_checks):
-        for index, value in enumerate(instructions):
+# milestones = []
+# accumulator_values = []
+# next_checks = [0]
+# visited_count = [0] * len(instructions)
+# for i, next_check in enumerate(next_checks):
+#         for index, value in enumerate(instructions):
+#             if any(x > 1 for x in visited_count):
+#                 break
+#             if next_check == index:
+#                 direction = value[:3]
+#                 n = value[4:]
+#                 milestones.append(value)
+#                 if direction == 'jmp':
+#                     accumulator_values.append(0)
+#                     next_checks.append(index + int(n))
+#                 elif direction == 'acc':
+#                     accumulator_values.append(int(n))
+#                     next_checks.append(index + 1)
+#                 else: # nop
+#                     accumulator_values.append(0)
+#                     next_checks.append(index + 1)
+#                 visited_count[index] += 1
+
+#print(sum(accumulator_values[:-1]))
+
+# part two --------------------------------------------------------------------
+
+instructions_two = instructions
+
+def run_the_loop():
+    accumulator_values = []
+    next_checks = [0]
+    visited_count = [0] * len(instructions_two)
+
+    for i, next_check in enumerate(next_checks):
+        for index, value in enumerate(instructions_two):
             if any(x > 1 for x in visited_count):
-                break
+                return(0)
             if next_check == index:
                 direction = value[:3]
                 n = value[4:]
-                milestones.append(value)
                 if direction == 'jmp':
                     accumulator_values.append(0)
                     next_checks.append(index + int(n))
                 elif direction == 'acc':
                     accumulator_values.append(int(n))
                     next_checks.append(index + 1)
-                else: # nop
+                else:  # nop
                     accumulator_values.append(0)
                     next_checks.append(index + 1)
                 visited_count[index] += 1
+    return(sum(accumulator_values))
 
-print(sum(accumulator_values[:-1]))
+for i in range(len(instructions_two)):
+    if 'nop' in instructions_two[i]:
+        instructions_two[i] = instructions_two[i].replace('nop', 'jmp')
+        print(instructions_two[i], run_the_loop())
+        instructions_two[i] = instructions_two[i].replace('jmp', 'nop')
 
-# part two --------------------------------------------------------------------
+for i in range(len(instructions_two)):
+    if 'jmp' in instructions_two[i]:
+        instructions_two[i] = instructions_two[i].replace('jmp', 'nop')
+        print(instructions_two[i], run_the_loop())
+        instructions_two[i] = instructions_two[i].replace('nop', 'jmp')
+
 
