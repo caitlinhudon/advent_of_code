2635186ed5896f2c6d28ac2e16d8b96dedf19b61
Caitlin Hudon <caitlingarrett8@gmail.com>
2020-12-09 22:20:43 -0600

    day nine: puzzle two complete!

--- day_nine.py
+++ day_nine.py
@@ -8,8 +8,6 @@ combos = list(combinations(range(25), 2)) # 25 values from positions 0 to 24
 i_values = list(range(len(xmas_data)))
 
 is_a_sum = list([0] * (len(xmas_data)))
-print(is_a_sum)
-print(type(is_a_sum))
 
 for i in range(len(xmas_data)):
     if i > 24:
@@ -26,4 +24,17 @@ print(xmas_data[561])
 
 # part two --------------------------------------------------------------------
 
+# brute force ftw lately :shrug:
+def find_addends():
+    for combo_length in range(len(xmas_data)):
+        if combo_length > 2:
+            for i in range(len(xmas_data)):
+                if i > 2:
+                    data = xmas_data[i:(i+combo_length)]
+                    data = [int(d) for d in data]
+                    data_total = sum(data)
+                    if data_total == 70639851:
+                        return sorted(data)
 
+output_list = find_addends()
+output = output_list[0] + output_list[len(output_list) - 1]
